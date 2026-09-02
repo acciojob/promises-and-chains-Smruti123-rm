ca771a62088ede1634a4e29e1699e122e67900a2
Smruti Ranjan Mishra <140094639+Smruti123-rm@users.noreply.github.com>
2026-09-02 20:41:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+    const age = document.getElementById("age");
+    const name = document.getElementById("name");
+
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const ageValue = age.value.trim();
+      const nameValue = name.value.trim();
+
+      if (ageValue === "" || nameValue === "") {
+        alert("Please enter valid details.");
+        return;
+      }
+
+      const ageNumber = Number(ageValue);
+
+      const votingPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (ageNumber > 18) {
+            resolve();
+          } else {
+            reject();
+          }
+        }, 4000);
+      });
+
+      votingPromise
+        .then(() => {
+          alert(`Welcome, ${nameValue}. You can vote.`);
+        })
+        .catch(() => {
+          alert(`Oh sorry ${nameValue}. You aren't old enough.`);
+        });
+    });
